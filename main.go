54eec051467ecbe44aaac7a841f340738c0023ab
Jesse <jfogarty@tuta>
2021-05-01 23:17:25 -0400

    pushes for mobile

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
 	"github.com/jessefogarty/goscraper/webscraper"
 )
 
@@ -9,13 +13,25 @@ func main() {
 	var cbc_article string = "https://www.cbc.ca/news/canada/ottawa/ottawa-police-interprovincial-crossings-covid-19-1.5992615"
 
 	// initialize source with html field
-	source := webscraper.Webpage{
+	/*source := webscraper.Webpage{
 		Html: webscraper.Fetch(cbc_article),
+	}*/
+
+	var source struct {
+		Title       string
+		Description string
+		Html        string
+		Links       []string
+		doc         *goquery.Document
 	}
 
-	source.Links = webscraper.Urls(source.Html)
+	source.doc = webscraper.Fetch(cbc_article)
+	source.Links = webscraper.Urls(source.doc)
+	source.Html, _ = source.doc.Html()
 
-	webscraper.Metadata(source.Html)
+	webscraper.Metadata(source.doc)
 
-	//fmt.Println(source)
+	j, _ := json.Marshal(source)
+
+	fmt.Printf("%s", string(j))
 }
