fa702d1129a216a3cf38860e87343281f162f875
Jesse <jfogarty@tuta>
2021-05-08 21:24:41 -0400

    threading looking good

--- scraper2/scraper2.go
+++ scraper2/scraper2.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -13,37 +15,49 @@ func main() {
 
 	args := os.Args[:]
 
-	ch := make(chan string, 1) // input from GET
-	ch2 := make(chan string, 1)
-
 	if len(args) == 2 {
 
 		s := strings.Split(args[1], ",")
 
+		ch := make(chan Webpage, len(s)) // init buffer
+		var wg sync.WaitGroup
+
 		for i, v := range s {
 			if i != len(s) {
-				go get(v, ch)
-				go out(ch, ch2)
-				fmt.Printf("%d - %T | ", i, <-ch2)
+				wg.Add(1)
+				go get(v, i, ch, &wg)
 			}
 
 		}
+		wg.Wait(); close(ch)
+
+		articles := ExportData{}
+
+		for a := range ch {
+
+			articles.Articles = append(articles.Articles, a)
+
+		}
+
+		j, _ := json.Marshal(articles)
+
+		fmt.Printf("%s", j)
+
 
 	}
-	//close(ch)
 }
 
-func out(ch chan string, ch2 chan string) {
-	data := <-ch
-	ch2 <- data
-}
+func get(url string, i int, ch chan Webpage, wg *sync.WaitGroup) {
 
-func get(url string, ch chan string) {
+	defer wg.Done()
+
+	article := Webpage{InputOrder: i}
 
 	resp, e := http.Get(url)
 
 	if e != nil {
-		ch <- "0"
+		article.Html = "0"
+		ch <- article
 	}
 
 	defer resp.Body.Close()
@@ -51,10 +65,26 @@ func get(url string, ch chan string) {
 	doc, e := goquery.NewDocumentFromReader(resp.Body)
 
 	if e != nil {
-		ch <- "0"
+		article.Html = "0"
+		ch <- article
 	}
 
 	html, _ := doc.Html()
-	ch <- html
+	article.Html = html
 
+	ch <- article
+
+}
+
+// Webpage represents the article object for each url.
+// Html source is scraped and input queue is saved.
+type Webpage struct {
+	InputOrder int
+	Html       string
+}
+
+// Ths structure of data in transit.
+// From Webpage to WebscraperJSON.
+type ExportData struct {
+	Articles []Webpage
 }
